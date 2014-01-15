4eb959cff2c2319abceb3f079898d355225d4250
Mike Chappels <mke.chappels@gmail.com>
2014-01-15 15:51:26 +0000

    new background images

--- content/themes/digitalhub/js/background.js
+++ content/themes/digitalhub/js/background.js
@@ -1,5 +1,5 @@
 jQuery(function($){
-	var bgImgs = new Array("/content/themes/digitalhub/images/back_one.png","/content/themes/digitalhub/images/back_two.jpg","/content/themes/digitalhub/images/back_three.jpg");
+	var bgImgs = new Array("/content/themes/digitalhub/images/back_one.png","/content/themes/digitalhub/images/backgroundtest_02.jpg","/content/themes/digitalhub/images/backgroundtest_03.jpg");
 var selectedImage=Math.floor(Math.random()*(bgImgs.length));
 $('#bgimg').css("background-image", "url("+bgImgs[selectedImage]+")");  
 
