160f7cfb27a2190ecf8287dbc73ecbd007e22615
Jonathan Masters <jonathan_masters77@yahoo.co.uk>
2014-01-30 14:39:30 +0000

    Contacts page layout, awaiting maps

--- content/themes/digitalhub/page-contacts.php
+++ content/themes/digitalhub/page-contacts.php
@@ -29,7 +29,7 @@ get_header(); ?>
           if ( comments_open() || '0' != get_comments_number() ) :
             comments_template();
           endif;
-        ?>
+         ?>
   <?php endwhile; // end of the loop. ?>
 
     </main><!-- #main -->
