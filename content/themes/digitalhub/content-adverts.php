0928d2ed4d54796d5b2c160a9a3800e014ca82f6
Michael Bragg <michael@michaelbragg.net>
2014-01-30 14:58:55 +0000

    Updates link to wrap around adverts image

--- content/themes/digitalhub/content-adverts.php
+++ content/themes/digitalhub/content-adverts.php
@@ -5,7 +5,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('gallery__item'); ?>>
-
+<a href="<?php the_permalink(); ?>" rel="bookmark">
 <?php if ( has_post_thumbnail() ) { // check if the post has a Post ?>
   <aside class="gallery--image">
     <?php the_post_thumbnail(); ?>
@@ -18,7 +18,7 @@
 
 <?php } ?>
   <header class="adverts__header">
-    <h1 class="adverts__title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+    <h1 class="adverts__title"><?php the_title(); ?></h1>
     <p class="adverts__content"><?php echo __( 'View Info &amp; Examples', 'digitalhub' ); ?></p>
-  </header><!-- .entry-header -->
+  </header><!-- .entry-header --></a>
 </article>
