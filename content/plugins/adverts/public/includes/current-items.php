e8332038cef651ce3300413c5d9353f3543ac561
Michael Bragg <michael@michaelbragg.net>
2014-02-04 17:44:33 +0000

    Adds missing files from last merge

---
+++ content/plugins/adverts/public/includes/current-items.php
@@ -0,0 +1,24 @@
+<?php
+
+add_filter('pre_get_posts', 'per_category_basis');
+
+function additional_active_item_classes($classes = array(), $menu_item = false){
+    global $wp_query;
+
+    if(in_array('current-menu-item', $menu_item->classes)){
+        $classes[] = 'current-menu-item';
+    }
+
+    if ( $menu_item->post_name == 'adverts' && is_post_type_archive('adverts') ) {
+        $classes[] = 'current-menu-item';
+    }
+
+    if ( $menu_item->post_name == 'adverts' && is_singular('adverts') ) {
+        $classes[] = 'current-menu-item';
+    }
+
+
+    return $classes;
+}
+add_filter( 'nav_menu_css_class', 'additional_active_item_classes', 10, 2 );
+ ?>
