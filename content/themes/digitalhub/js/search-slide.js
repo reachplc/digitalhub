303a97bdba47c2f24164e8970282a80148dac3b4
Jonathan Masters <jonathan_masters77@yahoo.co.uk>
2014-02-05 11:16:05 +0000

    Fxied issue with 2 search bars showing on browser resize

--- content/themes/digitalhub/js/search-slide.js
+++ content/themes/digitalhub/js/search-slide.js
@@ -1,10 +1,22 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function($) {  
 	
 	$('button.search').on('click' , function() {
 		$('button.search').toggleClass('clicked');
 		$('.wrapper #search-slide').slideToggle();
 	});
-	$('#search-close').on('click' , function() {
+	
+    $('#search-close').on('click' , function() {
 		$('.wrapper #search-slide').slideToggle();
 	});
 });
+
+
+;(function( $ ){
+  $( window ).resize(function() {
+    var wi = $(window).width();
+      if (wi >= 768){
+            $('.wrapper #search-slide').hide();
+            $('button.search').removeClass('clicked');
+            }
+  });
+})( jQuery );
