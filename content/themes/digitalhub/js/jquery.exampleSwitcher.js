99d5d47dc69037ccd21b454c8f174839bfc46194
Michael Bragg <michael@michaelbragg.net>
2014-02-06 10:44:56 +0000

    Adds interaction to thumbnail
    When a thumbnail is clicked the video will toggle and auto play.
    Indication is added to show which video is playing and which are not
    
    Ref: #59

--- content/themes/digitalhub/js/jquery.exampleSwitcher.js
+++ content/themes/digitalhub/js/jquery.exampleSwitcher.js
@@ -25,28 +25,90 @@ var exampleSwitch = exampleSwitch || {};
       $.extend(exampleSwitch.config, config);
     }
 
-    var _clicked = $('.js-example-preview').on( 'click', function() {
+    var _clicked = $('.js-example-preview').on( 'click', exampleSwitch.on);
 
-      var _holder = $( '.example__video' ).parent(),
-          _children = _holder.children('.example__video'),
-          _selected = $( this ).data('example');
+  };
 
-          for ( var i = 0; i < _children.length; i++ ) {
-              // Logs "try 0", "try 1", ..., "try 4".
-              var _current = i+1;
-              console.log( i, _current, _selected );
+  exampleSwitch.on = function() {
+    var _example = $( this ).children('img'),
+        _holder = $( '.example__video' ).parent(),
+        _children = _holder.children('.example__video'),
+        _selected = _example.data('example'),
+        _example_parent = _example.parents('.example__preview ul');
 
-              if( _selected === _current ) {
-                $('.example__video[data-example=' + _current + ']').removeClass('hidden');
-              }
-              if( _selected !== _current ) {
-                $('.example__video[data-example=' + _current + ']').addClass('hidden');
-              }
-          }
+    exampleSwitch.preview(_example, _example_parent);
+
+    for ( var i = 0; i < _children.length; i++ ) {
+
+        var _current = i+1;
+
+        if( _selected === _current ) {
+          exampleSwitch.video(_current, 'play');
+          $('.example__video[data-example=' + _current + ']').removeClass('hidden');
+        }
+
+        if( _selected !== _current ) {
+          exampleSwitch.video(_current, 'pause');
+          $('.example__video[data-example=' + _current + ']').addClass('hidden');
+        }
+
+    }
+
+  };
+
+  exampleSwitch.preview = function(example, parent) {
+
+    var _parent = parent.children('li');
+
+    $.each(_parent, function(){
+      _this = $(this),  // Cache the DOM lookup
+      _current = _this.children('img').data('example'),
+      _clicked = example.data('example');
+
+      //  Remove the icon on the selected element
+      if ( _current === _clicked ) {
+        _this.children('.icon').addClass('hidden');
+        _this.addClass('example__preview--selected');
+      }
+
+      //  Add the icon on all non-selected elements
+      if ( _current !== _clicked ) {
+        _this.children('.icon').removeClass('hidden');
+        _this.removeClass('example__preview--selected');
+      }
 
     });
 
+    exampleSwitch.video = function(video, action) {
+
+      var _video = $('.example__video[data-example="' + video + '"] video').get(0),
+          _action = action;
+
+        //console.log(_video, _action);
+
+        switch ( action ) {
+          case 'play':
+            if (_video.paused) {
+              _video.play();
+            }
+            if(_video.play){
+
+            }
+            break;
+          case 'pause':
+            if (_video.play) {
+              _video.pause();
+            }
+            if (_video.pause) {
+
+            }
+            break;
+        }
+
+    };
+
   };
+
 
 /**
  *  Code to be placed on web page to initialise the plugin.
