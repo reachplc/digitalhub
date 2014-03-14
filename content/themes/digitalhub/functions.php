079e04dafae17bc1bb0d67aa41d06466e2155e14
Michael Bragg <michael@michaelbragg.net>
2014-03-14 15:57:05 +0000

    Adds jQuery to array

--- content/themes/digitalhub/functions.php
+++ content/themes/digitalhub/functions.php
@@ -100,7 +100,7 @@ function digitalhub_scripts() {
 
   #wp_enqueue_script( 'digitalhub-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
-  wp_enqueue_script( 'digitalhub-global', get_template_directory_uri() . '/js/global.js', array(), '20130314', true );
+  wp_enqueue_script( 'digitalhub-global', get_template_directory_uri() . '/js/global.js', array('jquery'), '20130314', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
