defc835ab015f99ab6be77ad800a11a9846607cf
Michael Bragg <michael@michaelbragg.net>
2014-07-01 14:36:18 +0100

    Enables build guide media loader in settings page

--- content/plugins/adverts/admin/assets/js/admin.js
+++ content/plugins/adverts/admin/assets/js/admin.js
@@ -1,9 +1,42 @@
 (function ( $ ) {
-	"use strict";
+  "use strict";
 
-	$(function () {
+  jQuery(document).ready(function($){
 
 		// Place your administration-specific JavaScript here
+
+    var custom_uploader;
+
+    $('#advert-upload').click(function(e) {
+
+        e.preventDefault();
+
+        //If the uploader object has already been created, reopen the dialog
+        if (custom_uploader) {
+            custom_uploader.open();
+            return;
+        }
+
+        //Extend the wp.media object
+        custom_uploader = wp.media.frames.file_frame = wp.media({
+            title: 'Choose Image',
+            button: {
+                text: 'Choose Image'
+            },
+            multiple: false
+        });
+
+        //When a file is selected, grab the URL and set it as the text field's value
+        custom_uploader.on('select', function() {
+            var attachment = custom_uploader.state().get('selection').first().toJSON();
+            //console.log(attachment);
+            $('#new-setting #file-id').val(attachment.id);
+        });
+
+        //Open the uploader dialog
+        custom_uploader.open();
+
+    });
 
 	});
 
--- content/plugins/adverts/admin/views/admin.php
+++ content/plugins/adverts/admin/views/admin.php
@@ -17,6 +17,26 @@
 
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
+<?php // Check tthat the user is allowed to update options
+  if (!current_user_can('manage_options')) {
+    wp_die('You do not have sufficient permissions to access this page.');
+  }
+?>
+
 	<!-- @TODO: Provide markup for your options page here. -->
+
+  <form method="post" action="options.php">
+
+  <p>
+    <input id="advert-upload" name="advert-submit" class="button" type="button" value="Add Image">
+  </p>
+  <p id="new-setting">
+  <input id="file-id" type="text" name="fileid">
+  </p>
+
+
+  <?php submit_button(); ?>
+
+  </form>
 
 </div>
