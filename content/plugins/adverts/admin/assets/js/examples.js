3d674dea98a4c5a4a930d9fd4a26f92d0d39a7a5
Michael Bragg <michael@michaelbragg.net>
2014-09-30 15:38:38 +0100

    Add video template functionality for example plugin

--- content/plugins/adverts/admin/assets/js/examples.js
+++ content/plugins/adverts/admin/assets/js/examples.js
@@ -66,10 +66,8 @@ if ( undefined !== file_frame ) {
     $( output )
       .val( json.id );
 
+    if( $(output).is( '.js-data-thumbnail' ) ) {
     // Get url to use for preview
-
-
-
     // Build image
     var img = $('<img>', {
                   src: attach_url(json)
@@ -77,8 +75,6 @@ if ( undefined !== file_frame ) {
                  ,title: json.title
               })
               .appendTo( $( output ).closest('.js-example-item').find( '.js-preview-thumbnail' ) );
-
-    if ( $(output).is( '.js-data-thumbnail' ) ) {
     // Next, hide the anchor responsible for allowing the user to select an image
     $( '#js-add-thumbnail' )
         .hide();
@@ -87,18 +83,53 @@ if ( undefined !== file_frame ) {
         .show();
     }
 
-    if ( $(output).is( '#js-data-item' ) ) {
+    if( $(output).is( '#js-data-item' ) ) {
+      // Get url to use for preview
+      // Build image
+      var img = $('<img>', {
+                    src: attach_url(json)
+                   ,alt: json.caption
+                   ,title: json.title
+                })
+                .appendTo( $( output ).closest('.js-example-item').find( '.js-preview-thumbnail' ) );
 
-        var $_container = output.closest('.js-example-item');
+      var $_container = output.closest('.js-example-item');
 
-        $_container.find('#js-remove-image').show();
-        $_container.find('#js-add-image').hide();
-
-        console.log( 'output', $_container );
+      $_container.find('#js-remove-image').show();
+      $_container.find('#js-add-image').hide();
 
     }
 
-    //console.log('output', output);
+    if( $(output).is( '.js-data-placeholder' ) ) {
+      // Get url to use for preview
+      // Build image
+      var img = $('<img>', {
+                    src: attach_url(json)
+                   ,alt: json.caption
+                   ,title: json.title
+                })
+                .appendTo( $( output ).closest('.js-example-item').find( '.js-preview-thumbnail' ) );
+
+      var $_container = output.closest('.js-example-item');
+
+      $_container.find('.js-remove-placeholder').show();
+      $_container.find('.js-add-placeholder').hide();
+
+    }
+
+    if( $(output).is( '.js-data-mp4') ){
+      var $_container = output.closest('.js-example-item');
+      $_container.find( '.js-add-mp4' ).hide();
+      $_container.find( '.js-remove-mp4' ).show();
+    }
+
+    if( $(output).is( '.js-data-webm') ){
+      var $_container = output.closest('.js-example-item');
+      $_container.find( '.js-add-webm' ).hide();
+      $_container.find( '.js-remove-webm' ).show();
+    }
+
+    //console.log('output', $(output));
 
   });
 
@@ -141,14 +172,16 @@ function addImage ( $ ) {
 function addVideo ( $ ) {
   var imageTemplate = '<div class="js-example-item" style="margin-bottom: 2em; padding-bottom: 1em; border-bottom: 1px solid #dedede"><fieldset><h4>Item</h4>'
                     + '<button class="js-example-remove">Remove Example</button>'
-                    + '<input id="js-data-item" name="_example_item[][placeholder]" type="text">'
-                    + '<input id="js-data-item" name="_example_item[][mp4]" type="text">'
-                    + '<input id="js-data-item" name="_example_item[][webm]" type="text">'
+                    + '<input class="js-data-placeholder" name="_example_item[][placeholder]" type="text">'
+                    + '<input class="js-data-mp4" name="_example_item[][mp4]" type="text">'
+                    + '<input class="js-data-webm" name="_example_item[][webm]" type="text">'
                     + '<div class="js-preview-thumbnail"></div>'
-                    + '<p><button id="js-add-placeholder" class="button-secondary">Add Placeholder</button>'
-                    + '<button id="js-remove-placeholder" class="button-secondary" style="display: none;">Remove Placeholder</button>'
-                    + '<button id="js-add-mp4" class="button-secondary">Add MP4</button>'
-                    + '<button id="js-add-webm" class="button-secondary">Add WebM</button>'
+                    + '<p><button class="js-add-placeholder button-secondary">Add Placeholder</button>'
+                    + '<button class="js-remove-placeholder button-secondary" style="display: none;">Remove Placeholder</button>'
+                    + '<button class="js-add-mp4 button-secondary">Add MP4</button>'
+                    + '<button class="js-remove-mp4 button-secondary" style="display: none;">Remove MP4</button>'
+                    + '<button class="js-add-webm button-secondary">Add WebM</button>'
+                    + '<button class="js-remove-webm button-secondary" style="display: none;">Remove WebM</button>'
                     + '</p>'
                     + '</fieldset></div>';
   $( '#js-example-holder' ).append( imageTemplate );
@@ -223,6 +256,84 @@ function addVideo ( $ ) {
 
     });
 
+    // Add event listener for video placeholder
+
+    $( '#js-example-holder' ).on( 'click', '.js-add-placeholder', function( e ){
+      // Stop the default behaviour
+      e.preventDefault();
+      // Get the input field to add the media id
+      var field = $(this).closest('.js-example-item')
+                         .find( '.js-data-placeholder' );
+      // Display the media uploader
+      renderMediaUploader( $, field );
+
+    });
+
+    // Add event listener to remove image
+    $( '#js-example-holder' ).on( 'click', '.js-remove-placeholder', function( e ){
+      // Stop the default behaviour
+      e.preventDefault();
+
+      var $_container = $(this).closest('.js-example-item');
+
+      $_container.find('.js-data-placeholder').val( '' );
+      $_container.find( 'img' ).remove();
+      $_container.find('.js-remove-placeholder').hide();
+      $_container.find('.js-add-placeholder').show();
+
+    });
+
+    // Add event listener for mp4
+
+    $( '#js-example-holder' ).on( 'click', '.js-add-mp4', function( e ){
+      // Stop the default behaviour
+      e.preventDefault();
+      // Get the input field to add the media id
+      var field = $(this).closest('.js-example-item')
+                         .find( '.js-data-mp4' );
+      // Display the media uploader
+      renderMediaUploader( $, field );
+
+    });
+
+    // Add event listener to remove mp4
+    $( '#js-example-holder' ).on( 'click', '.js-remove-mp4', function( e ){
+      // Stop the default behaviour
+      e.preventDefault();
+
+      var $_container = $(this).closest('.js-example-item');
+
+      $_container.find('.js-data-mp4').val( '' );
+      $_container.find('.js-remove-mp4').hide();
+      $_container.find('.js-add-mp4').show();
+
+    });
+
+    // Add event listener for webm
+
+    $( '#js-example-holder' ).on( 'click', '.js-add-webm', function( e ){
+      // Stop the default behaviour
+      e.preventDefault();
+      // Get the input field to add the media id
+      var field = $(this).closest('.js-example-item')
+                         .find( '.js-data-webm' );
+      // Display the media uploader
+      renderMediaUploader( $, field );
+
+    });
+
+    // Add event listener to remove webm
+    $( '#js-example-holder' ).on( 'click', '.js-remove-webm', function( e ){
+      // Stop the default behaviour
+      e.preventDefault();
+
+      var $_container = $(this).closest('.js-example-item');
+
+      $_container.find('.js-data-webm').val( '' );
+      $_container.find('.js-remove-webm').hide();
+      $_container.find('.js-add-webm').show();
+
+    });
 
     /**
      * New Items
