2ff7173c75ac721be3684f46d3b62cef094716a5
Michael Bragg <michael@michaelbragg.net>
2014-10-09 13:13:58 +0100

    Add new video examples to be displayed with old examples video fallback

--- content/plugins/adverts/public/includes/example.php
+++ content/plugins/adverts/public/includes/example.php
@@ -49,7 +49,6 @@ function get_example_preview() {
   $old_example_items = get_post_meta( $post->ID, '_example_1_url_video', TRUE );
 
   if ( isset( $example_item ) ) {
-    //print_r($example_item);
 
     // Loop all examples
     foreach ( $example_item as $key=>$item ) {
@@ -102,55 +101,112 @@ function get_example_preview() {
  *
  */
 
-function get_example_video($custom) {
+function get_example_video() {
 
   global $post;
+  $custom = get_post_custom( $post->ID );
+  $example_thumbnail = json_decode( get_post_meta($post->ID, '_example_thumbnail', TRUE ) );
+  $example_item = json_decode( get_post_meta($post->ID, '_example_item', TRUE ) );
+
+
+  if ( isset( $example_item ) ) {
+
+    //print_r( $example_item );
+
+    foreach ( $example_item as $key=>$item ) {
+
+      if( !empty( $item->image ) ) {
+        echo '<div class="example__video';
+        if($key != 0){
+          echo' hidden';
+        };
+        echo '" data-example="' . $key . '">';
+
+        echo '</div>';
+      }
+
+      if( !empty( $item->video->placeholder ) ) {
+        echo '<div class="example__video';
+        if($key != 0){
+          echo' hidden';
+        };
+        echo '" data-example="' . $key . '">';
+        echo '<div class="video-wrap">';
+        echo '<canvas width="634" height="355"></canvas>';
+
+        // Add thumbnail image
+        echo '<video poster="' . wp_get_attachment_url( $example_thumbnail ) . '" controls>';
+        // Add mp4 video
+        if( !empty( $item->video->mp4 ) ) {
+          echo '<source src="' . wp_get_attachment_url( $item->video->mp4 ) . '">';
+        }
+        // Add web video
+        if( !empty( $item->video->webm ) ) {
+          echo '<source src="' . wp_get_attachment_url( $item->video->webm ) . '">';
+        }
+
+        echo '</video>';
+        echo '</div>';
+        echo '</div>';
+      }
+
+    }
+    // To stop the old examples being rendered
+    return;
+  }
+
+  /* To be depreciated in 0.3.0 */
+
+  $old_example_items = get_post_meta( $post->ID, '_example_1_url_video', TRUE );
   $_example_limit = 3;
   $_formats = array( 'mp4', 'webm' );
 
-  // Build each example
-  for ($i = 1; $i <= $_example_limit; $i++) {
+  if ( isset( $old_example_items ) ) {
 
-    // Build each video
-    if(!empty($custom['_example_' . $i . '_url_video']) && $custom['_example_' . $i . '_url_video'][0] != '0') {
+    // Build each example
+    for ($i = 1; $i <= $_example_limit; $i++) {
 
-      echo '<div class="example__video';
-      if($i != 1){
-        echo' hidden';
-      };
-      echo '" data-example="' . $i . '">';
-      // This makes our thumbnail image backwards compatible with 0.1.0
-      if( !isset( $custom['_example_thumbnail'][0] ) ) {
-        $_example_video = urldecode( $custom['_example_' . $i . '_url_video'][0] );
-      } else {
-      // Version 0.2.0
-        $_example_video = wp_get_attachment_url( $custom['_example_thumbnail'][0] );
-      }
+      // Build each video
+      if(!empty($custom['_example_' . $i . '_url_video']) && $custom['_example_' . $i . '_url_video'][0] != '0') {
+
+        echo '<div class="example__video';
+        if($i != 1){
+          echo' hidden';
+        };
+        echo '" data-example="' . $i . '">';
+        // This makes our thumbnail image backwards compatible with 0.1.0
+        if( !isset( $custom['_example_thumbnail'][0] ) ) {
+          $_example_video = urldecode( $custom['_example_' . $i . '_url_video'][0] );
+        } else {
+        // Version 0.2.0
+          $_example_video = wp_get_attachment_url( $custom['_example_thumbnail'][0] );
+        }
 
 
-      echo '<div class="video-wrap">';
+        echo '<div class="video-wrap">';
 
-      echo '<canvas width="634" height="355"></canvas>';
-      echo '<video poster="' . $_example_video . '" controls>';
+        echo '<canvas width="634" height="355"></canvas>';
+        echo '<video poster="' . $_example_video . '" controls>';
 
-      // Return each video if available
-      foreach ($_formats as $value) {
-        if(!empty($custom['_example_' . $i . '_url_'.$value][0])) {
-          $_format_type = urldecode( $custom['_example_' . $i . '_url_'.$value][0] );
-          echo '<source src="' . $_format_type . '">';
+        // Return each video if available
+        foreach ($_formats as $value) {
+          if(!empty($custom['_example_' . $i . '_url_'.$value][0])) {
+            $_format_type = urldecode( $custom['_example_' . $i . '_url_'.$value][0] );
+            echo '<source src="' . $_format_type . '">';
+          };
+
         };
 
-      };
+        unset($value);
 
-      unset($value);
+        echo '<div class="alert alert--message alert--info"><p><strong>Video Not Available.</strong> The browser you are using does not support HTML5 video. Please try viewing this page in an updated browser.</p></div>';
+        echo '</video>';
+        echo '</div>';
+        echo '</div>';
 
-      echo '<div class="alert alert--message alert--info"><p><strong>Video Not Available.</strong> The browser you are using does not support HTML5 video. Please try viewing this page in an updated browser.</p></div>';
-      echo '</video>';
-      echo '</div>';
-      echo '</div>';
+      }
 
     }
-
   }
 
 }
